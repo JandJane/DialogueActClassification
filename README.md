73ad41504aa65d40edd42fe3a500647c208a6a7a
Evgeniia Trufanova <foggyjandjane@gmail.com>
2023-04-19 18:13:10 +0200

    add comparisons

--- README.md
+++ README.md
@@ -1,23 +1,9 @@
+
 # Dialogue Act Classification
 
-PyTorch implementation of Dialogue Act Classification using BERT and RNN with Attention. 
+Implementation and comparison of several solutions for Dialogue Act Classification.
 
-### Implementation details
-
-BERT backbone is used to obtain utterance-level representations. BERT is fine-tuned for the first 3 epochs and then its weights are freezed.
-
-GRU with Attention is used to process a sequence of utterances. Then, a linear layer with softmax is applied to get final predictions.
-
-This implementation is partially based on:
-- [Pierre Colombo, Emile Chapuis, Matteo Manica, Emmanuel Vignon, Giovanna Varni, Chloe Clavel, "Guiding attention in Sequence-to-sequence models for Dialogue Act prediction", arXiv:2002.08801](https://arxiv.org/abs/2002.08801)
-- [Vipul Raheja, Joel Tetreault, "Dialogue Act Classification with Context-Aware Self-Attention", arXiv:1904.02594](https://arxiv.org/abs/1904.02594)
-
-The main differences between this implementation and the papers:
-- For utterance representations BERT contextualized embeddings are used instead of GloVe/ELMo/fastText with RNN.
-- Vanilla softmax instead of CRF.
-- Another type of Attention is used.
-
-### Dataset
+## Dataset
 
 [The Switchboard Dialog Act Corpus (SwDA)](https://catalog.ldc.upenn.edu/LDC97S62) is used for training.
 
@@ -25,10 +11,22 @@ The main differences between this implementation and the papers:
 
 Data is split into train, valid and test subsets according to ["Sequential Short-Text Classification with Recurrent and Convolutional Neural Networks"](http://arxiv.org/abs/1603.03827) NAACL 2016 paper.
 
-### Reproducing the results
+##  Results
+
+```markdown
+| Model                                      | Accuracy, % |
+|--------------------------------------------|-------------|
+| Tf-Idf + LightGBM without context          | 63.89       |
+| Fasttext + LightGBM without context        | 66.57       |
+| Pretrained Bert + LightGBM without context | 66.61       |
+| Fasttext + Hierarchical RNN                | 76.63       |
+| Pretrained Bert + RNN                      | 76.56       |
+| Fine-tuned Bert + RNN                      | 78.05       |
+```
+
+## Reproducing the results
 
 1. Clone the repo: `git clone --recurse-submodules https://github.com/JandJane/DialogueActClassification.git`
 2. Unzip data: `unzip DialogueActClassification/swda/swda.zip -d DialogueActClassification/swda/swda`
 3. Install requirements: `pip install -r DialogueActClassification/requirements.txt`
-4. Run `train_model.ipynb` notebook (training takes about 6 hours on Google Colab)
-
+4. Run notebooks 01-07
--- dialogue_classification/models.py
+++ dialogue_classification/models.py
@@ -19,8 +19,16 @@ torch.backends.cudnn.deterministic = True
 INF = torch.tensor(100_000).float().to(device)
 EPS = 1e-5
     
-    
+
+######################################## DIALOGUE RNN ########################################
+
 class DialogueRNN(nn.Module):
+    """
+    Inputs: 
+        - utterance_embs (batch_size, dialogue_len, input_size): utterance embeddings
+    Returns:
+        - preds (batch_size, dialogue_len, output_size): predictions, logits for each utterance
+    """
     def __init__(self, input_size=300, output_size=10, rnn_hidden_size=64, use_layer_norm=True):
         super().__init__()
 
@@ -35,23 +43,78 @@ class DialogueRNN(nn.Module):
             nn.Linear(rnn_hidden_size, output_size),
         )
 
-    def forward(self, utterance_embs, input_lengths):
-        batch_size, seq_len = utterance_embs.size(0), utterance_embs.size(1)
-
-        # Masks for input sequences
-        indexes = torch.arange(0, seq_len).expand(batch_size, seq_len)
-        mask = indexes < input_lengths.view(-1, 1)
-        mask = mask.to(device)
-
+    def forward(self, utterance_embs):
         rnn_output, _ = self.rnn(utterance_embs)
 
         if self.use_layer_norm:
             rnn_output = self.norm(rnn_output)
 
-        return self.head(rnn_output)
+        preds = self.head(rnn_output)
+        return preds
+    
 
+######################################## FASTTEXT HIERARCHICAL RNN ########################################
+                        
+class RNNEmbedder(nn.Module):
+    """
+    Inputs: 
+        - inputs (batch_size, dialogue_len, max_utterance_len, emb_dim): word embeddings, inputs for NN.
+    Returns:
+        - rnn_output (batch_size, dialogue_len, 2 * hidden_size): utterance embeddings
+    """
+    def __init__(self, input_size=300, hidden_size=300, max_utterance_len=MAX_UTTERANCE_LEN):
+        super().__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.max_utterance_len = max_utterance_len
+        
+        self.rnn = nn.GRU(input_size, hidden_size, batch_first=True, bidirectional=True)
 
+    def forward(self, inputs):
+        batch_size, seq_len = inputs.size(0), inputs.size(1)
+        
+        _, rnn_output = self.rnn(
+            inputs.reshape(batch_size * seq_len, self.max_utterance_len, self.input_size)
+        )
+        rnn_output = rnn_output.transpose(0, 1).reshape(batch_size, seq_len, 2 * self.hidden_size)
+
+        return rnn_output
+    
+
+class DialogueNetFasttext(nn.Module):
+    """
+    Inputs: 
+        - inputs (batch_size, dialogue_len, max_utterance_len, emb_dim): word embeddings, inputs for NN.
+        - input_lengths: ignored
+        - labels: ignored
+    Returns:
+        - outputs (batch_size, dialogue_len, output_size): predictions, logits for each utterance
+    """
+    def __init__(self, input_size=300, output_size=10, rnn_hidden_size=64, use_layer_norm=True, max_utterance_len=MAX_UTTERANCE_LEN):
+        super().__init__()
+        
+        self.rnn_embedder = RNNEmbedder(input_size=input_size, hidden_size=rnn_hidden_size, max_utterance_len=max_utterance_len)
+        
+        self.dialogue_rnn = DialogueRNN(input_size=2 * rnn_hidden_size, output_size=output_size, rnn_hidden_size=rnn_hidden_size,
+                                        use_layer_norm=use_layer_norm)
+
+    def forward(self, inputs, input_lengths=None, labels=None):
+        fasttext_embs = self.rnn_embedder(inputs)
+        outputs = self.dialogue_rnn(fasttext_embs)
+        return outputs
+    
+
+######################################## BERT RNN ########################################
+    
 class BertEmbedder(nn.Module):
+    """
+    Inputs: 
+        - input_ids (batch_size, dialogue_len, max_utterance_len): input token ids
+        - attention_masks (batch_size, dialogue_len, max_utterance_len): Bert attention masks
+    Returns:
+        - outputs (batch_size, dialogue_len, 768): Bert embeddings for each utterance
+    """
     def __init__(self, bert_layers_to_finetune=0, max_utterance_len=MAX_UTTERANCE_LEN):
         super().__init__()
         
@@ -75,37 +138,25 @@ class BertEmbedder(nn.Module):
         ).last_hidden_state
         attention_masks = attention_masks.reshape(-1, self.max_utterance_len, 1)
         bert_output = (bert_output * attention_masks).sum(dim=1) / (attention_masks.sum(dim=1) + EPS)
-        bert_output = bert_output.view(batch_size, seq_len, -1)   # Pooling ?
+        bert_output = bert_output.view(batch_size, seq_len, -1)
         
         return bert_output
 
     def freeze_bert(self):
         for param in self.bert.parameters():
             param.requires_grad = False
-            
-            
-class RNNEmbedder(nn.Module):
-    def __init__(self, input_size=300, hidden_size=300, max_utterance_len=MAX_UTTERANCE_LEN):
-        super().__init__()
-        
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.max_utterance_len = max_utterance_len
-        
-        self.rnn = nn.GRU(input_size, hidden_size, batch_first=True, bidirectional=True)
-
-    def forward(self, inputs):
-        batch_size, seq_len = inputs.size(0), inputs.size(1)
-        
-        _, rnn_output = self.rnn(
-            inputs.reshape(batch_size * seq_len, self.max_utterance_len, self.input_size)
-        )
-        rnn_output = rnn_output.transpose(0, 1).reshape(batch_size, seq_len, 2 * self.hidden_size)
-
-        return rnn_output
 
 
 class DialogueNetBert(nn.Module):
+    """
+    Inputs: 
+        - input_ids (batch_size, dialogue_len, max_utterance_len): input token ids
+        - attention_masks (batch_size, dialogue_len, max_utterance_len): Bert attention masks
+        - input_lengths: ignored
+        - labels: ignored
+    Returns:
+        - outputs (batch_size, dialogue_len, output_size): predictions, logits for each utterance
+    """
     def __init__(self, input_size=100, output_size=10, rnn_hidden_size=64, bert_layers_to_finetune=0,
                  use_layer_norm=True, max_utterance_len=MAX_UTTERANCE_LEN):
         super().__init__()
@@ -115,23 +166,8 @@ class DialogueNetBert(nn.Module):
         self.dialogue_rnn = DialogueRNN(input_size=768, output_size=output_size, rnn_hidden_size=rnn_hidden_size, 
                                         use_layer_norm=use_layer_norm)
 
-    def forward(self, input_ids, attention_masks, input_lengths, labels=None):
+    def forward(self, input_ids, attention_masks, input_lengths=None, labels=None):
         bert_embs = self.bert_embedder(input_ids, attention_masks)
-        outputs = self.dialogue_rnn(bert_embs, input_lengths)
+        outputs = self.dialogue_rnn(bert_embs)
         return outputs
-    
-    
-class DialogueNetFasttext(nn.Module):
-    def __init__(self, input_size=300, output_size=10, rnn_hidden_size=64, use_layer_norm=True, max_utterance_len=MAX_UTTERANCE_LEN):
-        super().__init__()
-        
-        self.rnn_embedder = RNNEmbedder(input_size=input_size, hidden_size=rnn_hidden_size, max_utterance_len=max_utterance_len)
-        
-        self.dialogue_rnn = DialogueRNN(input_size=2 * rnn_hidden_size, output_size=output_size, rnn_hidden_size=rnn_hidden_size,
-                                        use_layer_norm=use_layer_norm)
-
-    def forward(self, inputs, input_lengths, labels=None):
-        fasttext_embs = self.rnn_embedder(inputs)
-        outputs = self.dialogue_rnn(fasttext_embs, input_lengths)
-        return outputs
-        
+   
